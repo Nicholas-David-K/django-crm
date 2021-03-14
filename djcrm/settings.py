d183afa2727084d3f86125b5ba9122e7cdb43e57
Nicholas David <onedavid.nicholas@gmail.com>
2021-03-14 18:09:28 +0300

    Updated Dubug

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '5b5a3d25405c2da887ee529a0c4ccbd2ed9c35f5248dacdd43'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get("DEBUG_VALUE") == "True")
+DEBUG = (os.environ.get("DEBUG_VALUE") == "False")
 
 ALLOWED_HOSTS = ['djacrm.herokuapp.com']
 
